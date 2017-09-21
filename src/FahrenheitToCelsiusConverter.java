7eb722be1aa0045c6b7de942f1365abfd34b0df2
Michael <mich279q@edu.easj.dk>
2017-09-21 14:38:33 +0200

    Indsat fahrenheit til celsius uden Scanner

--- src/FahrenheitToCelsiusConverter.java
+++ src/FahrenheitToCelsiusConverter.java
@@ -8,5 +8,9 @@ public class FahrenheitToCelsiusConverter {
         double celsiusRes = (5.0/9) * (fahrenheit - 32);
         System.out.println("Fahrenheit valgt: " + fahrenheit);
         System.out.println(fahrenheit + " fahrenheit i celsius er " + celsiusRes + " grader!");
+
+        /* double fahrenheitTo = 104;
+        double celsiusResTo = (5.0/9) * (fahrenheitTo - 32);
+        System.out.println(fahrenheitTo + " fahrenheit i celsius er " + celsiusResTo + " grader!"); */
     }
 }
